b94c32d686fee59ec0af6d2dc2d9faab47eb3e98
Brendan <brendan@eurisko.con.au>
2021-08-17 11:07:38 +0800

    fixed colour picker
    Added Iris to backend colour inputs:
    https://automattic.github.io/Iris/

--- app/assets.php
+++ app/assets.php
@@ -12,11 +12,12 @@ class Assets
 
 		wp_enqueue_style('Reviews-reviews2', plugin_dir_url( __FILE__ ) . 'assets/css/style.php' );
 
-		wp_enqueue_style( 'wp-color-picker' );
+		// wp_enqueue_style( 'wp-color-picker' );
 
 		wp_enqueue_script('iris-min-js', plugin_dir_url( __FILE__ ) . 'assets/js/iris.min.js', array('jquery'), '',  true );
 
-		wp_enqueue_script('wp-color-picker-alpha', plugin_dir_url( __FILE__ ) . 'assets/js/wp-color-picker-alpha.js',  array( 'wp-color-picker' ), '',  true );
+		// wp_enqueue_script('wp-color-picker-alpha', plugin_dir_url( __FILE__ ) . 'assets/js/wp-color-picker-alpha.js',  array( 'wp-color-picker' ), '',  true );
+		wp_enqueue_script( 'colour-picker-js', plugin_dir_url( __FILE__ ) . 'assets/js/colour-picker.js', array('iris'), false, true );
 
 		wp_enqueue_script('Reviews-reviews-custom-js', plugin_dir_url( __FILE__ ) . 'assets/js/custom.js', array('jquery'),        '',  true );	
 
