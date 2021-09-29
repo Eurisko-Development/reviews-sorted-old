1d88b876d796ddfa1158f4ed07a37705354bd427
Brendan <brendan@eurisko.con.au>
2021-09-29 11:44:01 +0800

    #18
    Slider fix

--- app/views/slider.php
+++ app/views/slider.php
@@ -14,38 +14,41 @@
 		 ?>
 		<li class="testimonial-slide-item" itemscope itemtype="https://schema.org/Review">
 			<div>
-				<div class="testimonial-slide-speech" itemprop="itemReviewed" itemscope itemtype="https://schema.org/LocalBusiness">
+				<div class="testimonial-slide-speech">
+					<div itemprop="itemReviewed" itemscope itemtype="https://schema.org/LocalBusiness">
+						<meta itemprop="name" content="<?php bloginfo('name'); ?>">	
+						<meta itemprop="image" content="<?php echo get_site_icon_url();?>">	
+						<meta itemprop="address" content="<?php echo $option['business_address'] ?>">	
+						<meta itemprop="telephone" content="<?php echo $option['business_phone'] ?>">	
+						<meta itemprop="priceRange" content="$$$">					
+					</div>
 					<div class="testimonial-slide-speech-text" itemprop="reviewBody">
-					<?php 
-					$max_str_length = $option['testimonial_character_length'] ?? 180;
-					$testi_content = nl2br($review->content);
+						<?php 
+						$max_str_length = $option['testimonial_character_length'] ?? 180;
+						$testi_content = nl2br($review->content);
 
-						if (strlen($testi_content) >= $max_str_length) {
-							echo substr($testi_content, 0, $max_str_length). " ... ";
-						}
-						else {
-							echo $testi_content;
-						}
-						?>
-					</div>	
-					
-					
-					<meta itemprop="name" content="<?php bloginfo('name'); ?>">	
-					<meta itemprop="image" content="<?php echo get_site_icon_url();?>">	
-					<meta itemprop="address" content="<?php echo $option['business_address'] ?>">	
-					<meta itemprop="telephone" content="<?php echo $option['business_phone'] ?>">	
-					<meta itemprop="priceRange" content="$$$">					
+							if (strlen($testi_content) >= $max_str_length) {
+								echo substr($testi_content, 0, $max_str_length). " ... ";
+							}
+							else {
+								echo $testi_content;
+							}
+							?>
+					</div>
 				</div>
-				<span class="testimonial-slide-author" itemprop="author" itemscope itemtype="https://schema.org/Person">
-					<strong itemprop="name"><?= $review->authorfname."&nbsp;".$review->authorlname ?></strong>, <!--?= $review->state ?-->
+				<div class="testimonial-slide-author">
+					<span  itemprop="author" itemscope itemtype="https://schema.org/Person">
+						<strong itemprop="name"><?= $review->authorfname."&nbsp;".$review->authorlname ?></strong>, 
+					</span> <!--?= $review->state ?-->
 					<span class="testimonial-region">
-						<?= $review->state ?></span> - <?= date('d M Y', strtotime($review->created_at)) ?><br>
+						<?= $review->state ?>
+					</span> - <?= date('d M Y', strtotime($review->created_at)) ?><br>
 					<span class="testimonial-stars testimonial-stars-<?php echo $av_stars; ?>" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
 						<span><?= $review->rating ?> Stars</span>
-						<meta itemprop="ratingValue" content="<?php echo $av_stars; ?>">	
+						<meta itemprop="ratingValue" content="<?php echo $av_stars; ?>">
 						<meta itemprop="bestRating" content="5">	
 					</span>
-				</span>
+				<div>
 			</div>
 		</li>
 		<?php endforeach; ?>
